92ebfb568c09d4e41bdd16b921bd98d525e61d90
frontlineqsr-png <frontlineqsr@gmail.com>
2026-02-18 21:33:54 -0700

    Update firebase.js

--- assets/firebase.js
+++ assets/firebase.js
@@ -1,5 +1,5 @@
 // assets/firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
@@ -13,6 +13,6 @@ const firebaseConfig = {
   measurementId: "G-8KFEXHE1TR"
 };
 
-export const app = initializeApp(firebaseConfig);
+export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
